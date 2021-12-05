fe587b1a1150a05e0def01a4eb9bb98e24ead799
felipecassimirodev <fcassimiro94@gmail.com>
2021-12-05 02:20:06 -0300

    criação de api teste

---
+++ MOVEISAPI/Model/MoviesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MOVEISAPI.Model
+{
+    public class MoviesDbContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+        }
+    }
+}
